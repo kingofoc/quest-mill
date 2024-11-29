3fdfe36d65aba914075d70dc46f9a3ff92dbcb15
kingofoc <kingofoc202@gmail.com>
2024-11-29 18:30:33 +0100

    initial commit

---
+++ src/app/page.js
@@ -0,0 +1,20 @@
+import React from "react";
+import UserTGData from "@/components/UserTGData";
+
+
+export default function Home() {
+  return (
+    <div className="container">
+      {/* Header content */}
+      <header>
+        <div>
+          <UserTGData />
+        </div>
+      </header>
+
+      <main>
+
+      </main>
+    </div>
+  );
+}
