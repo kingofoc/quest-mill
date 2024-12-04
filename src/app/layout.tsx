b681b0f0a31c13bac46c8290b391e36db6a626b5
kingofoc <kingofoc202@gmail.com>
2024-12-04 18:52:37 +0100

    second commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,6 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import Script from "next/script";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Quest Mill",
@@ -28,7 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
       <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet" />
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         {children}
       </body>
     </html>
--- src/components/Spin.tsx
+++ src/components/Spin.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Image from "next/image"
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react"
 
 const coinImage = "/assets/coin.png";
 
