aa7a22e60c16ba3bf7c6ffb3291f64b31c873e39
kingofoc <kingofoc202@gmail.com>
2024-12-05 10:00:08 +0100

    another commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const homeIcon = "/assets/house-solid.svg";
 const earnIcon = "/assets/task.svg";
@@ -10,18 +11,18 @@ export default function Footer () {
     return (
         <div>
             <footer className="footer-container">
-                <a href="/">
+                <Link href="/">
                     <Image className="footer-icon" src={homeIcon} alt="" width={24}  height={24}/>
-                </a>
-                <a href="">
+                </Link>
+                <Link href="">
                     <Image className="footer-icon" src={earnIcon} alt="" width={24} height={24} />
-                </a>
-                <a href="">
+                </Link>
+                <Link href="">
                     <Image className="footer-icon" src={rankingIcon} alt="" width={24} height={24}/>
-                </a>
-                <a href="">
+                </Link>
+                <Link href="">
                     <Image className="footer-icon" src={referIcon} alt="" width={24} height={24} />
-                </a>
+                </Link>
             </footer>
         </div>
     )
