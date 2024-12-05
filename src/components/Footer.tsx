1769162797a041d93889e0bc8b9ff998cbca7523
kingofoc <kingofoc202@gmail.com>
2024-12-05 10:48:52 +0100

    another commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,22 +6,31 @@ const homeIcon = "/assets/house-solid.svg";
 const earnIcon = "/assets/task.svg";
 const rankingIcon = "/assets/ranking.svg";
 const referIcon = "/assets/users.svg";
+const airdropIcon = "/assets/earn.svg";
 
 export default function Footer () {
     return (
         <div>
             <footer className="footer-container">
-                <Link href="/">
+                <Link href="/" className="footer-link">
                     <Image className="footer-icon" src={homeIcon} alt="" width={24}  height={24}/>
+                    <p>Home</p>
                 </Link>
-                <Link href="">
+                <Link href="" className="footer-link">
                     <Image className="footer-icon" src={earnIcon} alt="" width={24} height={24} />
+                    <p>Tasks</p>
                 </Link>
-                <Link href="">
+                <Link href="" className="footer-link">
                     <Image className="footer-icon" src={rankingIcon} alt="" width={24} height={24}/>
+                    <p>Ranking</p>
                 </Link>
-                <Link href="">
+                <Link href="" className="footer-link">
                     <Image className="footer-icon" src={referIcon} alt="" width={24} height={24} />
+                    <p>Friends</p>
+                </Link>
+                <Link href="" className="footer-link">
+                    <Image className="footer-icon" src={airdropIcon} alt="" width={24} height={24}/>
+                    <p>Airdrop</p>
                 </Link>
             </footer>
         </div>
